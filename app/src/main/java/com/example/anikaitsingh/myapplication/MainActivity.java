edddcc5be1b45e9e023ba77dc1b195b5778dbf38
Anikait Singh <singh.anikait@gmail.com>
2018-04-11 06:55:50 -0400

    App Changes

--- app/src/main/java/com/example/anikaitsingh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/anikaitsingh/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void contactUs(View view){
         Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-                "mailto","singh.anikait@gmail.com", null));
+                "mailto","wwptechdesk@gmail.com", null));
         intent.putExtra(Intent.EXTRA_SUBJECT, "HELP!");
         intent.putExtra(Intent.EXTRA_TEXT, "My issue is ");
         startActivity(Intent.createChooser(intent, "Choose an Email client :"));
@@ -25,8 +26,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void showToastAboutApp(View view){
         String message = "This app helps you with technology at WW-P South."
-                +"Made by Anikait Singh.";
+                +" Made by Anikait Singh.";
         Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+    public void showFAQ(View view){
+        Intent intent = new Intent(this, FAQActivity.class);
+        this.startActivity(intent);
     }
 }
